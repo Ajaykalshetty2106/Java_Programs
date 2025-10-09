7c4288f6ec0390eb1ca8523f7e8b9fd09aa20a05
Ajaykalshetty2106 <ajaykalshetty2106@gmail.com>
2025-10-09 11:40:24 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,14 @@
+package storemarks;
+
+public class marks {
+	public static void main(String[] arg) {
+	int maths = 70 ;
+	int science = 75 ;
+	int english = 78 ;
+	float total = maths + science + english ;
+	float average = total / 3 ;
+	System.out.println("Total marks is " + total);
+	System.out.println("Average marks is " + average);
+	}
+		
+}
